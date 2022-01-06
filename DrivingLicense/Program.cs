57d1d2fe9c24d3473917b2b7eec2f4b79ce01d8c
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-06 19:45:57 +0200

    Update Program.cs

--- DrivingLicense/Program.cs
+++ DrivingLicense/Program.cs
@@ -10,6 +10,34 @@ namespace DrivingLicense
     {
         static void Main(string[] args)
         {
+            //programm kusib kasutaja sunniaasta;
+            //programm arvutab kasutaja vanuse;
+            //kui kasutaja on noorem kui 18, siis programm kuvab konsoolis;
+            //"oled liiga noor, et juhilube saada"
+            //kui kasutaja on vanem kui 18, siis programm kuvab konsoolis;
+            //"oled piisavalt vana, et juhilube saada"
+            //kui kasutaja on 18, siis programm kuvab konsoolis;
+            //"Palju onne! Nuud sa saada ka juhiluba taotleda";
+
+            Console.WriteLine("Palun sisesta oma sunniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            Console.WriteLine("Sa oled " + userAge + "aastat vana.");
+
+            if (userAge < 18)
+            {
+                Console.WriteLine("Oled liiga noor, et juhilube saada!");
+
+            }
+            else
+            {
+                Console.WriteLine("Palju onne ! Nuud saad ka omale juhiloa taotleda.");
+            }
+
+
+            Console.WriteLine("Kena paeva jatku");
         }
     }
 }
